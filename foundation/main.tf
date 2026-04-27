95ed82faa72d445715439c4f31101c3a3ba4e413
Luke <campbelllu3@gmail.com>
2026-04-26 17:21:48 -0700

    Foundational layers built: VPC, 1 public subnet, IGW, RT's connected. State saved successfully to S3/Dynamo, prevents duplicate infra changes via state locking.

--- foundation/main.tf
+++ foundation/main.tf
@@ -17,10 +17,9 @@ resource "aws_s3_bucket_versioning" "enabled" {
   }
 }
 
-#Need to add dynamo privs to this user if you want terraform to make this
 # Create a DynamoDB table for state locking
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-up-and-running-locks"
+  name         = "terraform-running-locks"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
@@ -30,5 +29,63 @@ resource "aws_dynamodb_table" "terraform_locks" {
   }
 }
 
+# VPC is the blank stadium upon which all IDP's shall be painted
+resource "aws_vpc" "stadium" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
-#Need to add master VPC below this, home for all the IDP-subnets
+  tags = {
+    Name = "vpc-0"
+  }
+}
+
+# IGW
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.stadium.id
+
+  tags = { Name = "igw-0" }
+}
+
+# Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.stadium.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-rt-0"
+  }
+}
+
+# public subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.stadium.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-0"
+  }
+}
+
+# RT Associated with public_subnet
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+
+# For reference or debugging later; run terraform apply to print to screen; also saves ID's to s3 for later programmatical calls to their ID's
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.stadium.id
+}
+
+output "public_subnet_id" {
+  description = "The ID of the original subnet"
+  value       = aws_subnet.public_subnet.id
+}
--- foundation/provider.tf
+++ foundation/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket         = "idp-tf-state-storage"
+    key            = "foundation/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-running-locks"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
