92ba4f5cf6917e3c7eaa3199bf09077d86f086fb
Luke <campbelllu3@gmail.com>
2026-05-25 13:06:15 -0700

    Modules edited to match SSM ingress. Final review this week before Go Code being written.

--- modules/idp-env/variables.tf
+++ modules/idp-env/variables.tf
@@ -1,47 +1,36 @@
 variable "env_name" {
   type        = string
-  description = "Name of the developer environment (e.g. dev-luke)"
+  description = "The name of this dev environment (e.g. dev-luke)"
 }
 
 variable "vpc_id" {
   type        = string
   description = "The ID of our Foundation VPC"
-}
-
-variable "instance_type" {
-  default = "t3.micro"
-}
-
-
-
-#for later
-variable "env_name" {
-  type        = string
-  description = "The name of this dev environment"
-}
-
-variable "vpc_id" {
-  type        = string
-  description = "The ID of the master VPC"
-}
+} 
 
 variable "subnet_id" {
   type        = string
   description = "The ID of the subnet where the EC2 will live"
 }
 
-#This will be commented out as SSM comes up
-variable "dev_ip_address" {
-  type        = string
-  description = "Your home IP for the security group (e.g. 1.2.3.4/32)"
-}
-
 variable "instance_type" {
   type    = string
   default = "t3.micro"
+  description = "The size of the virtual server to deploy"
 }
 
-variable "key_name" {
-    type = string
-    description = "Which SSH Key Pair to Use for access"
-}
+# ==============================================================
+# DEPRECATED VARIABLES (Kept for documentation/portfolio reference)
+# ==============================================================
+
+#This will be commented out as SSM comes up
+# variable "dev_ip_address" {
+#   type        = string
+#   description = "Your home IP for the security group (e.g. 1.2.3.4/32)"
+# }
+
+# Deprecated due to ingress change from networking to SSM
+# variable "key_name" {
+#     type = string
+#     description = "Which SSH Key Pair to Use for access"
+# }
