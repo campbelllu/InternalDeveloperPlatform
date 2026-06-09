512e2a94f97f04956fa5451a2dc82aa18da745ec
Luke <campbelllu3@gmail.com>
2026-06-08 21:46:46 -0700

    Changed public subnet name to reflect architectural decisions.

--- foundation/main.tf
+++ foundation/main.tf
@@ -61,14 +61,14 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
-# public subnet
+# public subnet, simulating corporate test env already existing
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.stadium.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "public-subnet-0"
+    Name = "test-env-0"
   }
 }
 
